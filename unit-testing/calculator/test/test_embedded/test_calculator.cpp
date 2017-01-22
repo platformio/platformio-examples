e6e0c19bd1149ae7b5e5b8f72c9ccc98f1690441
Ivan Kravets <me@ikravets.com>
2017-01-23 00:12:35 +0200

    Add new options  ``--monitor-rts`` and ``--monitor-dtr`` to `pio test` command

--- unit-testing/calculator/test/test_embedded/test_calculator.cpp
+++ unit-testing/calculator/test/test_embedded/test_calculator.cpp
@@ -32,6 +32,10 @@ void test_function_calculator_division(void) {
 }
 
 void setup() {
+    // NOTE!!! Wait for >2 secs
+    // if board doesn't support software reset via Serial.DTR/RTS
+    delay(2000);
+
     UNITY_BEGIN();
     RUN_TEST(test_function_calculator_addition);
     RUN_TEST(test_function_calculator_subtraction);
