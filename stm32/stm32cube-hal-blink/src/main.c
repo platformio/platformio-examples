a30800d4aa9a5841645f3425b8abccc1d46064bb
Valerii Koval <valeros@bigmir.net>
2017-06-12 16:11:06 +0300

    Add new examples with STM32Cube framework

---
+++ stm32/stm32cube-hal-blink/src/main.c
@@ -0,0 +1,90 @@
+#if F0
+#include "stm32f0xx_hal.h"
+#elif F1
+#include "stm32f1xx_hal.h"
+#elif F2
+#include "stm32f2xx_hal.h"
+#elif F3
+#include "stm32f3xx_hal.h"
+#elif F4
+#include "stm32f4xx_hal.h"
+#elif F7
+#include "stm32f7xx_hal.h"
+#elif L0
+#include "stm32l0xx_hal.h"
+#elif L1
+#include "stm32l1xx_hal.h"
+#elif L4
+#include "stm32l4xx_hal.h"
+#else
+#error "Unsupported STM32 Family"
+#endif
+
+#define LED_PIN                                GPIO_PIN_5
+#define LED_GPIO_PORT                          GPIOA
+#define LED_GPIO_CLK_ENABLE()                  __HAL_RCC_GPIOA_CLK_ENABLE()
+
+int main(void)
+{
+  HAL_Init();
+  
+  LED_GPIO_CLK_ENABLE();
+  
+  GPIO_InitTypeDef GPIO_InitStruct;
+  
+  GPIO_InitStruct.Pin = LED_PIN;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+  HAL_GPIO_Init(LED_GPIO_PORT, &GPIO_InitStruct); 
+
+  while (1)
+  {
+    HAL_GPIO_TogglePin(LED_GPIO_PORT, LED_PIN);
+    
+    HAL_Delay(1000);
+  }
+}
+
+void SysTick_Handler(void)
+{
+  HAL_IncTick();
+}
+
+void NMI_Handler(void)
+{
+}
+
+void HardFault_Handler(void)
+{
+  while (1) {}
+}
+
+
+void MemManage_Handler(void)
+{
+  while (1) {}
+}
+
+void BusFault_Handler(void)
+{
+  while (1) {}
+}
+
+void UsageFault_Handler(void)
+{
+  while (1) {}
+}
+
+void SVC_Handler(void)
+{
+}
+
+
+void DebugMon_Handler(void)
+{
+}
+
+void PendSV_Handler(void)
+{
+}
