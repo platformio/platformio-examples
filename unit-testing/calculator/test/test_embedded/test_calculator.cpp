18c0d44404215089c1d02d60a2a700bd5f7d1673
Ivan Kravets <me@ikravets.com>
2022-04-23 11:11:47 +0300

    Add "IGNORED" case

--- unit-testing/calculator/test/test_embedded/test_calculator.cpp
+++ unit-testing/calculator/test/test_embedded/test_calculator.cpp
@@ -28,19 +28,19 @@ Calculator calc;
 // // clean stuff up here
 // }
 
-void test_function_calculator_addition(void) {
+void test_calculator_addition(void) {
     TEST_ASSERT_EQUAL(32, calc.add(25, 7));
 }
 
-void test_function_calculator_subtraction(void) {
+void test_calculator_subtraction(void) {
     TEST_ASSERT_EQUAL(20, calc.sub(23, 3));
 }
 
-void test_function_calculator_multiplication(void) {
+void test_calculator_multiplication(void) {
     TEST_ASSERT_EQUAL(50, calc.mul(25, 2));
 }
 
-void test_function_calculator_division(void) {
+void test_calculator_division(void) {
     TEST_ASSERT_EQUAL(32, calc.div(100, 3));
 }
 
@@ -50,10 +50,10 @@ void setup() {
     delay(2000);
 
     UNITY_BEGIN();
-    RUN_TEST(test_function_calculator_addition);
-    RUN_TEST(test_function_calculator_subtraction);
-    RUN_TEST(test_function_calculator_multiplication);
-    RUN_TEST(test_function_calculator_division);
+    RUN_TEST(test_calculator_addition);
+    RUN_TEST(test_calculator_subtraction);
+    RUN_TEST(test_calculator_multiplication);
+    RUN_TEST(test_calculator_division);
     UNITY_END();
 }
 
